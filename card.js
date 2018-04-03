b15b7515d397efc79e46d58961c88c07e655db57
dorazouri <dorazouri@gmail.com>
2018-04-04 00:42:26 +0300

    cards files renaming

--- card.js
+++ card.js
@@ -1,6 +1,32 @@
 function Card(color, number, action) {
-  this.color = color;  // 
-  this.number = number;  // null if card is not a regular number card
   this.action = action;  // null if card is a regular number card
+  this.number = number;  // null if card is not a regular number card
+  this.color = color;  // null if card is not colored
+
+  this.getFileName = function(){
+  	var text = "";
+  	if (null != this.action){
+  		text += this.action.toString();
+  	} else {
+  		text += 0;
+  	}
+  	text += "_";
+
+  	if (null != this.number){
+  		text += this.number.toString();
+  	} else {
+  		text += 0;
+  	}
+  	text += "_";
+
+  	if (null != this.color){
+  		text += this.color.toString();
+  	} else {
+  		text += 0;
+  	}
+  	text += ".png";
+
+	return text;
+  }
 }
 
--- game.js
+++ game.js
@@ -6,19 +6,28 @@ window.onload = function () {
     cards = deckOfCards.init();
     console.log(cards);
 
-    var parent = document.getElementById("board");
+    var parent = document.getElementById("player-bottom-flex-container");
     console.log(parent);
+
+    console.log(cards.length)
+    playerCards = cards.slice(76);
+    console.log(playerCards.length)
+    computerCards = cards.slice(68,76);
+    console.log(computerCards.length)
+    cards = cards.slice(0,68);
+    console.log(cards.length)
 
 
     var DOMaddCardImgToDiv = function(card){
 	    var node = document.createElement("div");
 	    var child = parent.appendChild(node);
-	    console.log(child);
-	    child.innerHTML = "<img src=\"cards/card0002.png\"/>"
+	    // console.log(child);
+	    child.innerHTML = "<img src=\"cards/" + card.getFileName() + " \"/>"
 	    child.addEventListener('click', function(){alert('asdasd')})
+	    // console.log(card.getFileName());
 	};
 
 
-    cards.forEach(DOMaddCardImgToDiv);
+    playerCards.forEach(DOMaddCardImgToDiv);
 
 };
