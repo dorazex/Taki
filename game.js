4020a9e83a288b8c1cf6f0c6ffba1dba46b9f3f8
dorazouri <dorazouri@gmail.com>
2018-04-03 23:40:06 +0300

    initial population of cards in main deck

--- game.js
+++ game.js
@@ -5,4 +5,20 @@ window.onload = function () {
     var deckOfCards = new deck();
     cards = deckOfCards.init();
     console.log(cards);
+
+    var parent = document.getElementById("board");
+    console.log(parent);
+
+
+    var DOMaddCardImgToDiv = function(card){
+	    var node = document.createElement("div");
+	    var child = parent.appendChild(node);
+	    console.log(child);
+	    child.innerHTML = "<img src=\"cards/card0002.png\"/>"
+	    child.addEventListener('click', function(){alert('asdasd')})
+	};
+
+
+    cards.forEach(DOMaddCardImgToDiv);
+
 };
